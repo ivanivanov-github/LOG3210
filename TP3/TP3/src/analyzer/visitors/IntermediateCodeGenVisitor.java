8cc858ec0b69709b629a30822f7e91cab222a333
ivanivanov-gitlab <ivan.ivanov@polymtl.ca>
2023-03-02 15:45:06 -0500

    arith tests pass

--- TP3/TP3/src/analyzer/visitors/IntermediateCodeGenVisitor.java
+++ TP3/TP3/src/analyzer/visitors/IntermediateCodeGenVisitor.java
@@ -42,8 +42,10 @@ public class IntermediateCodeGenVisitor implements ParserVisitor {
 
     @Override
     public Object visit(ASTProgram node, Object data) {
-        node.childrenAccept(this, data);
+        String S_next = newLabel();
+        node.childrenAccept(this, S_next);
         // TODO
+        m_writer.print(S_next + "\n");
         return null;
     }
 
@@ -56,8 +58,14 @@ public class IntermediateCodeGenVisitor implements ParserVisitor {
 
     @Override
     public Object visit(ASTBlock node, Object data) {
-        node.childrenAccept(this, data);
+//        node.childrenAccept(this, data);
         // TODO
+        for (int i = 0; i < node.jjtGetNumChildren() - 1; i++) {
+            String S_next = newLabel();
+            node.jjtGetChild(i).jjtAccept(this, S_next);
+            m_writer.println(S_next);
+        }
+        node.jjtGetChild(node.jjtGetNumChildren() - 1).jjtAccept(this, data);
         return null;
     }
 
@@ -111,8 +119,10 @@ public class IntermediateCodeGenVisitor implements ParserVisitor {
     @Override
     public Object visit(ASTAssignStmt node, Object data) {
         String identifier = ((ASTIdentifier) node.jjtGetChild(0)).getValue();
-        node.jjtGetChild(1).jjtAccept(this, data);
+//        node.jjtGetChild(1).jjtAccept(this, data);
         // TODO
+        String addr = node.jjtGetChild(1).jjtAccept(this, data).toString();
+        m_writer.println(identifier + " = " + addr);
         return null;
     }
 
@@ -125,9 +135,19 @@ public class IntermediateCodeGenVisitor implements ParserVisitor {
         // À noter qu'il n'est pas nécessaire de boucler sur tous les enfants.
         // La grammaire n'accepte plus que 2 enfants maximum pour certaines opérations, au lieu de plusieurs
         // dans les TPs précédents. Vous pouvez vérifier au cas par cas dans le fichier Langage.jjt.
-        node.childrenAccept(this, data);
+//        node.childrenAccept(this, data);
         // TODO
-        return null;
+        if (ops.size() == 0) {
+            return node.jjtGetChild(0).jjtAccept(this, data);
+        } else {
+            String id = newID();
+
+            String id1 = (String) node.jjtGetChild(0).jjtAccept(this, data);
+            String id2 = (String) node.jjtGetChild(1).jjtAccept(this, data);
+
+            m_writer.print(id + " = " + id1 + " " + ops.firstElement() + " " + id2 + "\n");
+            return id;
+        }
     }
 
     @Override
@@ -142,37 +162,48 @@ public class IntermediateCodeGenVisitor implements ParserVisitor {
 
     @Override
     public Object visit(ASTUnaExpr node, Object data) {
-        node.jjtGetChild(0).jjtAccept(this, data);
+//        node.jjtGetChild(0).jjtAccept(this, data);
         // TODO
-        return null;
+        if (node.getOps().isEmpty()) {
+            return node.jjtGetChild(0).jjtAccept(this, data);
+        } else {
+            String id = (node.jjtGetChild(0).jjtAccept(this, data)).toString();
+            for (int i = 0; i < node.getOps().size(); i++) {
+                String id1 = newID();
+
+                m_writer.print(id1 + " = " + node.getOps().get(i) + " " + id + "\n");
+                id = id1;
+            }
+            return id;
+        }
     }
 
     @Override
     public Object visit(ASTBoolExpr node, Object data) {
-        node.childrenAccept(this, data);
+//        node.childrenAccept(this, data);
         // TODO
-        return null;
+        return node.jjtGetChild(0).jjtAccept(this, data);
     }
 
     @Override
     public Object visit(ASTCompExpr node, Object data) {
-        node.childrenAccept(this, data);
+//        node.childrenAccept(this, data);
         // TODO
-        return null;
+        return node.jjtGetChild(0).jjtAccept(this, data);
     }
 
     @Override
     public Object visit(ASTNotExpr node, Object data) {
-        node.jjtGetChild(0).jjtAccept(this, data);
+//        node.jjtGetChild(0).jjtAccept(this, data);
         // TODO
-        return null;
+        return node.jjtGetChild(0).jjtAccept(this, data);
     }
 
     @Override
     public Object visit(ASTGenValue node, Object data) {
-        node.jjtGetChild(0).jjtAccept(this, data);
+//        node.jjtGetChild(0).jjtAccept(this, data);
         // TODO
-        return null;
+        return node.jjtGetChild(0).jjtAccept(this, data);
     }
 
     @Override
